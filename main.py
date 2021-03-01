f3b693c481a74676f019c473e4cd6f593351bdf2
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-03-01 20:32:32 +0200

    added end game logic

--- main.py
+++ main.py
@@ -14,23 +14,33 @@ turtle.shape(image)
 data = pd.read_csv('50_states.csv')
 
 state_list = data['state'].to_list()
-answers = []
+guessed_states = []
 
 
-
-while len(answers) < 50:
+while len(guessed_states) < 50:
     #promt for user to gues
-    answer_state = screen.textinput(title=f'{len(answers)}/50 Guess the state',
+    answer_state = screen.textinput(title=f'{len(guessed_states)}/50 Guess the state',
                     prompt = "What's another state name?").title()
+    #break game is user types exit. Saves missing answers to another csv
+    if answer_state == "Exit":
+        missing_states = []
+        for state in state_list:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.csv('states_to_learn.csv')
+        break
     #check if the guess is in the list and write it on the map
     if answer_state in state_list:
-        answers.append(answer_state)
+        guessed_states.append(answer_state)
         state_name = data[data.state == answer_state]
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
         t.goto(int(state_name.x), int(state_name.y))
         t.write(answer_state)
+
+
 
 
 
