6fc9d23063cf0b66c33689e9d8995be2918b2144
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-03-01 20:08:20 +0200

    write state name on map if guess is right

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ screen.addshape(image)
 turtle.shape(image)
 
 answer_state = screen.textinput(title='Guess the state', prompt = "What's another state name?")
-print(answer_state)
 
 #read data from csv
 data = pd.read_csv('50_states.csv')
 
-#create a state list
+#create a state list with all states lower case
 state_list = data['state'].to_list()
-print(state_list)
+state_list = [state.lower() for state in state_list]
 
-
-
+#check if the guess is in the list and write it on the map
+if answer_state.lower() in state_list:
+    state_name = data[data.state == answer_state.capitalize()]
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    t.write(answer_state.capitalize())
+    t.goto(int(state_name.x), int(state_name.y))
 
 
 
